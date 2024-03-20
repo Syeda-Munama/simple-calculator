aa3b8460764d6a168e6d1f2f4d2a854227638e98
Syeda-Munama <syedamunamahassan@gmail.com>
2024-03-21 02:31:07 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,35 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  {
+    message: "Enter First Number:",
+    type: "number",
+    name: "num1",
+  },
+  {
+    message: "Enter Second Number:",
+    type: "number",
+    name: "num2",
+  },
+  {
+    message: "Select one of the operator for operation:",
+    type: "list",
+    name: "operator",
+    choices: ["+", "-", "*","/","%"],
+  }
+]);
+if (answer.operator === "+"){
+    console.log(answer.num1 + answer.num2);
+} else if (answer.operator === "-"){
+    console.log(answer.num1 - answer.num2);
+} else if (answer.operator === "*"){
+    console.log(answer.num1 * answer.num2);
+} else if (answer.operator === "/"){
+    console.log(answer.num1 / answer.num2);
+}  else if (answer.operator === "%"){
+  console.log(answer.num1 % answer.num2);
+}else {
+    console.log("Invalid Operator");
+}
